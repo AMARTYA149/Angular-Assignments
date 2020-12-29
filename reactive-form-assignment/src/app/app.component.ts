47a8456cf19c9c61e989639926b5b3e49b9d383c
Amartya Aishwarya <amartya970914@gmail.com>
2020-12-29 14:09:59 +0530

    reactive-form assignement

---
+++ reactive-form-assignment/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidator } from './custom-validators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(
+        null,
+        [Validators.required, CustomValidator.invalidProjectName],
+        CustomValidator.asyncInvalidProjectName
+      ),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      projectStatus: new FormControl('critical'),
+    });
+  }
+
+  onSaveProject() {
+    console.log(this.projectForm.value);
+  }
+}
